cd4cd98f83a045134c0c6b798047a941f21af94b
Hemant Sharma <hemantsharmafli7@gmail.com>
2026-08-25 19:51:24 +0530

    Infra configuration for render: dockerfile, start.sh, supervisord.conf to run all services as a single container inside render.

---
+++ redarky/start.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+
+# Default to 8000 if Render doesn't inject PORT
+export PORT="${PORT:-8000}"
+
+# Optional: Run Alembic migrations before spinning up services
+cd /app/redarky/api
+alembic upgrade head || true
+
+# Start Supervisor
+exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
