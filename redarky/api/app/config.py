568b5aacd4701e1d0a1354660b0af5d541c9319b
Hemant Sharma <hemantsharmafli7@gmail.com>
2026-08-28 06:19:03 +0530

    Updated CORS for frontend

--- redarky/api/app/config.py
+++ redarky/api/app/config.py
@@ -37,7 +37,7 @@ class Settings(BaseSettings):
     APIFY_WEBHOOK_URL: str = "http://localhost:8000/ingestion/reddit"
 
     # ALLOWED ORIGINS 
-    ALLOWED_ORIGINS: list = ["http://localhost:5173", "http://127.0.0.1:5173"]
+    ALLOWED_ORIGINS: list = ["http://localhost:5173", "http://127.0.0.1:5173", "https://redarky.vercel.app"]
 
     model_config = SettingsConfigDict(
         env_file="../.env",
